beaac09e8083f9b70db0107403a57f956c398838
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2017-03-31 17:35:27 +0700

    typo

--- app/helpers/command_worker.rb
+++ app/helpers/command_worker.rb
@@ -1,6 +1,6 @@
 class CommandWorker
   include Sidekiq::Worker
-  sidekig_options retry: false # job will be discarded immediately if failed
+  sidekiq_options retry: false # job will be discarded immediately if failed
 
   def perform(params)
     Commands::Processor.init(params).run
